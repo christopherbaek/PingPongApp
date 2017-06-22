235438be20620d5a89dade110c8d1e1f332e9e48
Christopher Baek <christopher.baek@gmail.com>
2017-06-21 18:03:30 -0700

    Add scaffolding for switch hooks

--- app/src/main/java/com/cbaek/pingpongapp/MainActivity.java
+++ app/src/main/java/com/cbaek/pingpongapp/MainActivity.java
@@ -1,7 +1,10 @@
 package com.cbaek.pingpongapp;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.widget.CompoundButton;
+import android.widget.Toast;
+import android.widget.ToggleButton;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,6 +12,22 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(final Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        // set up toggle button
+        final ToggleButton toggleButton = (ToggleButton) findViewById(R.id.toggle_button);
+        toggleButton.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+
+            @Override
+            public void onCheckedChanged(final CompoundButton buttonView, final boolean isChecked) {
+                if (isChecked) {
+                    Toast.makeText(MainActivity.this, "Starting service", Toast.LENGTH_LONG).show();
+                } else {
+                    Toast.makeText(MainActivity.this, "Stopping service", Toast.LENGTH_LONG).show();
+                }
+            }
+
+        });
+
     }
 
 }
