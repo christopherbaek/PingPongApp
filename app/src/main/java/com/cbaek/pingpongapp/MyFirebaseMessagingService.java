48dca47695260d124ea4fc0417e5f4be89ae0957
Christopher Baek <christopher.baek@gmail.com>
2017-06-22 17:47:07 -0700

    Display Firebase messages with a Toast

--- app/src/main/java/com/cbaek/pingpongapp/MyFirebaseMessagingService.java
+++ app/src/main/java/com/cbaek/pingpongapp/MyFirebaseMessagingService.java
@@ -1,6 +1,7 @@
 package com.cbaek.pingpongapp;
 
 import android.util.Log;
+import android.widget.Toast;
 
 import com.google.firebase.messaging.FirebaseMessagingService;
 import com.google.firebase.messaging.RemoteMessage;
@@ -25,15 +26,18 @@ public class MyFirebaseMessagingService extends FirebaseMessagingService {
         // TODO(developer): Handle FCM messages here.
         // Not getting messages here? See why this may be: https://goo.gl/39bRNJ
         Log.d(TAG, "From: " + remoteMessage.getFrom());
+        Toast.makeText(this, "From: " + remoteMessage.getFrom(), Toast.LENGTH_LONG).show();
 
         // Check if message contains a data payload.
         if (remoteMessage.getData().size() > 0) {
             Log.d(TAG, "Message data payload: " + remoteMessage.getData());
+            Toast.makeText(this, "Message data payload: " + remoteMessage.getData(), Toast.LENGTH_LONG).show();
         }
 
         // Check if message contains a notification payload.
         if (remoteMessage.getNotification() != null) {
             Log.d(TAG, "Message Notification Body: " + remoteMessage.getNotification().getBody());
+            Toast.makeText(this, "Message Notification Body: " + remoteMessage.getNotification().getBody(), Toast.LENGTH_LONG).show();
         }
 
         // Also if you intend on generating your own notifications as a result of a received FCM
